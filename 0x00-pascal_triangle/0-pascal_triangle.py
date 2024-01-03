01510c06cb47bc920fa07e00874f323ecc833b53
DzidediSenaya <dzidedisenaya@gmail.com>
2024-01-03 15:23:22 +0000

    finished task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the nth row.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
+
+
+# Example usage
+if __name__ == "__main__":
+    # Test with n = 5
+    triangle = pascal_triangle(5)
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
