81bafc58f18f6fdb7489d4d995a1100953d07230
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-04 19:41:13 +0000

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,54 +1,30 @@
 #!/usr/bin/python3
-"""
-Module for UTF-8 validation
-"""
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
-    """
-    Determines if a given data set represents a valid UTF-8 encoding.
-
-    Args:
-        data (list): List of integers representing the data.
-
-    Returns:
-        bool: True if data is a valid UTF-8 encoding, else False.
-    """
-    # Number of bytes in the current UTF-8 character
+    """determines if a given data set represents a valid UTF-8 encoding"""
     num_bytes = 0
-
-    # Iterate through each integer in the data
     for num in data:
-        # Check if the current integer is a continuation byte
         if num_bytes == 0:
+            # Check the number of leading set bits
             if (num >> 7) == 0b0:
                 continue
-            elif (num >> 5) == 0b110:
-                num_bytes = 1
-            elif (num >> 4) == 0b1110:
-                num_bytes = 2
-            elif (num >> 3) == 0b11110:
-                num_bytes = 3
-            else:
+            set_bits = 0
+            helper = 1 << 7
+            while helper & num:
+                set_bits += 1
+                helper = helper >> 1
+
+            # Validate the number of bytes based on leading set bits
+            if set_bits == 1 or set_bits > 4:
                 return False
+
+            num_bytes = set_bits - 1  # Set the remaining bytes to check
         else:
-            # Check if the current byte is a continuation byte
-            if (num >> 6) != 0b10:
+            # Check if the current byte has the format 10xxxxxx
+            if not (num & (1 << 7) and not (num & (1 << 6))):
                 return False
             num_bytes -= 1
 
-    # Check if there are any remaining bytes
     return num_bytes == 0
-
-
-# Example usage
-if __name__ == "__main__":
-    data = [65]
-    print(validUTF8(data))
-
-    data = [80, 121, 116, 104, 111, 110, 32,
-            105, 115, 32, 99, 111, 111, 108, 33]
-    print(validUTF8(data))
-
-    data = [229, 65, 127, 256]
-    print(validUTF8(data))
