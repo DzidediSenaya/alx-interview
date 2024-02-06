fa25c4d4f9321e06a95eaae5aedca61d64b96fca
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-02-06 19:52:01 +0000

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,64 +1,54 @@
 #!/usr/bin/python3
-"""
-N Queens solved with Backtracing
-"""
+"""Solving N Queens with Backtracing"""
 import sys
 
 
-def nqueens(n, row, board):
+def nqueens(n, y, board):
     """
     Method: nqueens - place n queens
             on an n by n board so that
             no queens are attacking any
             others.
-    Parameters:
-        n (int): board size and # of queens
-        row (int): current row
-        board (list): current board state
-    Returns: All possible solutions to
+    Parameters: n is an int that sets
+                board size and # of queens
+    Return: All possible solutions to
             placement, in list of lists form
     """
-    if row == n:
-        print(board)
-        return
-
-    for col in range(n):
-        if is_safe(board, row, col):
-            board.append([row, col])
-            nqueens(n, row + 1, board)
-            board.pop()
-
-
-def is_safe(board, row, col):
-    """
-    Check if it's safe to place a queen at board[row][col]
-    """
-    for q_row, q_col in board:
-        if col == q_col or row - col == q_row - q_col or row + col == q_row + q_col:
-            return False
-    return True
+    for x in range(n):
+        hold = 0
+        for q in board:
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
 
 
 def main():
-    """
-    Main function to solve N Queens problem
-    """
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
         n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
+    except Exception:
+        print('N must be a number')
         sys.exit(1)
-
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
     nqueens(n, 0, [])
-
 
 if __name__ == '__main__':
     main()
