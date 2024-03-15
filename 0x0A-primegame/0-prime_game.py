7c04c5fcf249d283e280a5bd3b949982c9f7b6b6
DzidediSenaya <dzidedisenaya@gmail.com>
2024-03-15 05:49:57 +0000

    updated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,6 +2,29 @@
 """Module to determine the winner of the prime game."""
 
 
+def isWinner(x, nums):
+    """Determine the winner of the prime game."""
+    if x <= 0 or not nums:
+        return None
+
+    wins_maria = 0
+    wins_ben = 0
+
+    for n in nums:
+        primes_count = sum(1 for i in range(1, n + 1) if is_prime(i))
+        if primes_count % 2 == 0:
+            wins_ben += 1
+        else:
+            wins_maria += 1
+
+    if wins_maria > wins_ben:
+        return "Maria"
+    elif wins_maria < wins_ben:
+        return "Ben"
+    else:
+        return None
+
+
 def is_prime(n):
     """Check if a number is prime."""
     if n <= 1:
@@ -16,29 +39,6 @@ def is_prime(n):
             return False
         i += 6
     return True
-
-
-def isWinner(x, nums):
-    """Determine the winner of the prime game."""
-    if x <= 0 or not nums:
-        return None
-
-    wins_maria = 0
-    wins_ben = 0
-
-    for n in nums:
-        primes_count = sum(1 for i in range(1, n+1) if is_prime(i))
-        if primes_count % 2 == 0:
-            wins_ben += 1
-        else:
-            wins_maria += 1
-
-    if wins_maria > wins_ben:
-        return "Maria"
-    elif wins_maria < wins_ben:
-        return "Ben"
-    else:
-        return None
 
 
 if __name__ == "__main__":
